4f4ae15a6358312f5b667eb4ea7560325d03bdfb
yyd48264 <1041153118@qq.com>
2018-10-15 13:43:51 +0800

    1

---
+++ MvcWebApi/WebAPI/Controllers/Home/BangDingJiLuController.cs
@@ -0,0 +1,82 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ModelDb;
+using Com;
+using System.Web;
+using WebAPI.Models;
+using System.Web.Http.Results;
+using System;
+
+namespace WebAPI.Controllers.Home
+{
+    public class BangDingJiLuController : ApiController
+    {
+        private EBMSystemEntities db = new EBMSystemEntities();
+        JsonModel model = new JsonModel();
+        public ResponseMessageResult Get()
+        {
+            string openid = HttpContext.Current.Request.Headers.GetValues("openid").First().ToString();
+            if (!string.IsNullOrEmpty(openid))
+            {
+                var temp = from a in db.BangDingJiLu
+                           join b in db.wechatUser on a.openid equals b.openid
+                           where a.iBangDingLeiXing == 1 && a.openid == openid
+                           select a;
+                model.data = temp.OrderByDescending(o=>o.dShiJian).FirstOrDefault();
+
+                if (model.data != null)
+                {
+                    model.message = "查询成功";
+                    model.status_code = 200;
+                    return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+                }
+                else
+                {
+                    model.message = "暂无数据";
+                    model.status_code = 200;
+                }
+            }
+            else
+            {
+                model.message = "微信授权失败";
+                model.status_code = 401;
+            }
+            return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+        }
+        public ResponseMessageResult Get(int iBangDingLeiXing)
+        {
+            string openid = HttpContext.Current.Request.Headers.GetValues("openid").First().ToString();
+            if (!string.IsNullOrEmpty(openid))
+            {
+                BangDingJiLu jilu = new BangDingJiLu();
+                jilu.iBangDingLeiXing = iBangDingLeiXing;
+                jilu.openid = openid;
+                jilu.iFangShi = 0;
+                jilu.dShiJian = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                db.BangDingJiLu.Add(jilu);
+                try
+                {
+                    db.SaveChanges();
+                    model.message = "新增成功";
+                    model.status_code = 200;
+                }
+                catch (Exception ex)
+                {
+                    model.message = ex.Message;
+                    model.status_code = 401;
+                }
+            }
+            else
+            {
+                model.message = "微信授权失败";
+                model.status_code = 401;
+            }
+            return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+        }
+    }
+}
---
+++ MvcWebApi/WebAPI/Controllers/Home/GongDiCheLiangController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ModelDb;
+using Com;
+using System.Web;
+using WebAPI.Models;
+using System.Web.Http.Results;
+
+namespace WebAPI.Controllers.Home
+{
+    public class GongDiCheLiangController : ApiController
+    {
+        private EBMSystemEntities db = new EBMSystemEntities();
+        JsonModel model = new JsonModel();
+
+        public ResponseMessageResult Get(string cGongDiBianMa, string keyword=null)
+        {
+            string openid = HttpContext.Current.Request.Headers.GetValues("openid").First().ToString();
+            if (!string.IsNullOrEmpty(openid))
+            {
+                var temp = from a in db.CheLiangInfo
+                           join b in db.wechatUser on a.openid equals b.openid
+                           join c in db.GongDiCheLiang on a.cChePaiHao equals c.cChePaiHao
+                           where (c.cGongDiBianMa == cGongDiBianMa || string.IsNullOrEmpty(cGongDiBianMa))
+                           && (a.cChePaiHao.Contains(keyword) || a.cPinPai.Contains(keyword) || b.cXingMing.Contains(keyword) || string.IsNullOrEmpty(keyword))
+                           select new
+                           {
+                               a.cChePaiHao,
+                               a.cPinPai,
+                               b.cXingMing,
+                               b.openid
+                           };
+                model.data = temp.ToList();
+
+                if (model.data != null)
+                {
+                    model.message = "查询成功";
+                    model.status_code = 200;
+                    return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+                }
+                else
+                {
+                    model.message = "暂无数据";
+                    model.status_code = 200;
+                }
+            }
+            else
+            {
+                model.message = "微信授权失败";
+                model.status_code = 401;
+            }
+            return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+        }
+    }
+}
