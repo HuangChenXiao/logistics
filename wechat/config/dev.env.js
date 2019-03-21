873e37c46c193e7b518da54b50a5848462170283
yyd48264 <1041153118@qq.com>
2019-03-21 19:15:57 +0800

    1

--- wechat/config/dev.env.js
+++ wechat/config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:59358/api/"',
-  WECHAT_API: '"http://localhost:59358/"',
-  // BASE_API: '"https://mobile.xmxtm.cn/api/API/"',
-  // WECHAT_API: '"https://mobile.xmxtm.cn/API/"',
+  // BASE_API: '"http://localhost:59358/api/"',
+  // WECHAT_API: '"http://localhost:59358/"',
+  BASE_API: '"https://mobile.xmxtm.cn/api/API/"',
+  WECHAT_API: '"https://mobile.xmxtm.cn/API/"',
   IMG_URL: '""',
 })
