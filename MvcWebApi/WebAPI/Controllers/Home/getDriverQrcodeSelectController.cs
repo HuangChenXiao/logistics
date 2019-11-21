996735c692464700257576a4f46414a4f6a80f10
huangcx <1041153118@qq.com>
2019-11-21 21:17:58 +0800

    1

---
+++ MvcWebApi/WebAPI/Controllers/Home/getDriverQrcodeSelectController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ModelDb;
+using Com;
+using System.Web;
+using WebAPI.Models;
+using System.Web.Http.Results;
+
+namespace WebAPI.Controllers.Home
+{
+    public class getDriverQrcodeSelectController : ApiController
+    {
+        private EBMSystemEntities db = new EBMSystemEntities();
+        JsonModel model = new JsonModel();
+
+        public ResponseMessageResult Get(string cChePaiHao)
+        {
+            string openid = HttpContext.Current.Request.Headers.GetValues("openid").First().ToString();
+            if (!string.IsNullOrEmpty(openid))
+            {
+                var temp = from a in db.CheLiangInfo join b in db.wechatUser on a.openid equals b.openid
+                           where a.cChePaiHao == cChePaiHao
+                           select new
+                           {
+                               a.cChePaiHao,
+                               b.cXingMing,
+                               a.openid
+                           };
+                model.data = temp.FirstOrDefault();
+
+                if (model.data != null)
+                {
+                    model.message = "查询成功";
+                    model.status_code = 200;
+                    return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+                }
+                else
+                {
+                    model.message = "该车辆暂未绑定驾驶员";
+                    model.status_code = 401;
+                }
+            }
+            else
+            {
+                model.message = "微信授权失败";
+                model.status_code = 401;
+            }
+            return new ResponseMessageResult(Request.CreateResponse((HttpStatusCode)model.status_code, model));
+        }
+    }
+}
